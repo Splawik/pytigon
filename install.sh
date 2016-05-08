28213f94f7156c67337b7d4416952efc50d4c365
Splawik <slawomir.cholaj@gmail.com>
2016-05-08 16:46:55 +0200

    small changes

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     ./python/bin/pip install  -r requirements.txt
     ./python/bin/python ./ext_prg/http -dco ./ext_prg/tcc-0.9.26.tar.bz2 http://download.savannah.gnu.org/releases/tinycc/tcc-0.9.26.tar.bz2
@@ -10,13 +11,27 @@ else
     curl -o python-3.5.1-embed-win32.zip https://www.python.org/ftp/python/3.5.1/python-3.5.1-embed-win32.zip
     mkdir python
     unzip python-3.5.1-embed-win32.zip -d ./python
+    rm python-3.5.1-embed-win32.zip
     unzip ./python/python35.zip -d ./python/Lib
     rm ./python/python35.zip
     curl -o ./python/get-pip.py https://bootstrap.pypa.io/get-pip.py
     ./python/python ./python/get-pip.py
     ./python/Scripts/pip install -r requirements.txt
+    ./python/Scripts/pip install -r requirements.txt
     curl -L -o ./python/lxml-3.4.4-cp35-none-win32.whl https://pypi.anaconda.org/giumas/simple/lxml/3.4.4/lxml-3.4.4-cp35-none-win32.whl
     ./python/Scripts/pip install ./python/lxml-3.4.4-cp35-none-win32.whl
+    curl -L -o ext_lib_cli_win.zip http://pytigon.tk/download/ext_lib_cli_win.zip
+    unzip ext_lib_cli_win.zip
+    rm ext_lib_cli_win.zip    
     curl -L -o ./ext_prg/tcc-0.9.26-win32-bin.zip http://download.savannah.gnu.org/releases/tinycc/tcc-0.9.26-win32-bin.zip
     unzip ./ext_prg/tcc-0.9.26-win32-bin.zip -d ./ext_prg
+    rm ./ext_prg/tcc-0.9.26-win32-bin.zip
+    curl -L -o ./ext_prg/LinkRes2Exe.zip http://pytigon.tk/download/LinkRes2Exe.zip
+    unzip ./ext_prg/LinkRes2Exe.zip -d ./ext_prg
+    rm ./ext_prg/LinkRes2Exe.zip
+    ./ext_prg/tcc pytigon.c -ladvapi32
+    ./ext_prg/LinkRes2Exe pytigon.res pytigon.exe
+    ./ext_prg/tcc pytigon_cmd.c -ladvapi32
+    ./ext_prg/LinkRes2Exe pytigon.res pytigon_cmd.exe
+    curl -L -o ./install/vcredist_x86.exe http://pytigon.tk/download/vcredist_x86.exe
 fi
