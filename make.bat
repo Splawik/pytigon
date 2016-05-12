addd8e19de6fc2ee5e85ba2ddc0e8fc2bbe501ff
Splawik <slawomir.cholaj@gmail.com>
2016-05-12 20:54:10 +0200

    small changes

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
 cd static_src\sch
-..\..\python\bin\python ..\..\python\lib\python3.5\site-packages\transcrypt\__main__.py -n pytigon.py
-..\..\python\bin\python install.py
+..\..\python\python ..\..\python\lib\site-packages\transcrypt\__main__.py -n pytigon.py
+..\..\python\python install.py
--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd static_src/sch
-../../python/python ../../python/lib/site-packages/transcrypt/__main__.py -n pytigon.py
-../../python/python install.py
+../../python/bin/python ../../python/lib/python3.5/site-packages/transcrypt/__main__.py -n pytigon.py
+../../python/bin/python install.py
