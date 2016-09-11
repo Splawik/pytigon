879a3b259a3da43c487ac8dab81f410ab3e8a589
Splawik <slawomir.cholaj@gmail.com>
2016-09-11 22:39:05 +0200

    small changes

---
+++ app_pack/schdevtools/global_db_settings.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import pymysql
+pymysql.install_as_MySQLdb()
+
+def setup_databases(app_name):
+
+    p = os.path.expanduser("~")
+    db_name = os.path.join(p, ".pytigon/%s/%s.db" % (app_name, app_name))
+
+    dbs =    {'default':  
+                    {  
+                        'ENGINE': 'django.db.backends.mysql',
+                        'NAME': 'sch',
+                        'USER': 'root',
+                        'PASSWORD': 'AnawaAnawa1',
+                        'HOST': 'pytigon.tk',
+                    },
+    }
+    #auth = (
+    #   'schserw.schsys.remotebackend.RemoteUserBackendMod',
+    #)
+    #return (dbs, auth)
+    return (dbs, None)
