2c49b62ba65894e8885692bb96b0077f9e66cce1
Splawik <slawomir.cholaj@gmail.com>
2017-01-29 19:16:08 +0100

    small changes

---
+++ __init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+#from .models import Scripts
+
+ModuleTitle = _('main')
+Title = _('Simple scripts')
+Perms = False
+Index = ''
+Urls  = [
+    ('table/Scripts/-/form/list?schtml=1',_('Scripts'),None,'client://apps/utilities-terminal.png'),
+]
+
+UserParam = {}
+
+def AdditionalUrls():
+    from .models import Scripts
+    ret = []
+    for object in Scripts.objects.all():
+        if object.menu:
+            elements = object.menu.split(',')
+            if len(elements)>2:
+                if elements[0]=='main':
+                    if len(elements)>3:
+                        ret.append(('/simplescripts/run/'+object.name, elements[1], None, elements[2]))
+                    else:
+                        ret.append(('/simplescripts/run/'+object.name, elements[1], None, 'client://apps/utilities-terminal.png'))
+    return ret
