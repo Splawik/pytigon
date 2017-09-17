2c8f1f61a74ac78bb9d554403ff12b7015c12422
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2017-09-17 20:55:18 +0200

    small changes

---
+++ schlib/schdjangoext/fastform.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+#Pytigon - wxpython and django application framework
+
+#author: "Slawomir Cholaj (slawomir.cholaj@gmail.com)"
+#copyright: "Copyright (C) ????/2012 Slawomir Cholaj"
+#license: "LGPL 3.0"
+#version: "0.1a"
+
+"""
+Form example in string format
+Co to jest?
+Nazwisko
+Nazwisko2:*
+imie/Imię!:*
+Opis:_
+Data:####.##.##
+Wiek:0
+Kwota!:9.99
+Wybierz:[opcja1;opcja2
+opcja3]
+"""
+
+
+def _scan_lines(input_str):
+    l = input_str.replace('\r','').split('\n')
+    ret = []
+    append_to_last = False
+    for pos in l:
+        if append_to_last:
+            ret[-1]=ret[-1]+";"+pos
+            if ']' in pos:
+                append_to_last=False
+        else:
+            ret.append(pos)
+            if ':[' in pos and not ']' in pos:
+                append_to_last = True
+    return ret
+
+
+def _get_name_and_title(s):
+    required = False
+    if s.endswith('!'):
+        required = True
+        s = s[:-1]
+        
+    if '/' in s:
+        name, title = s.split('/', 1)
+    else:
+        title = s
+        x = s.encode('ascii', 'replace').decode('utf-8').replace('?','_').lower()
+        name = ""
+        for z in x:
+            if (z>='a' and z<='z') or (z>='0' and z<='9') or z=='_':
+                name += z
+        name = name[:16]
+    return name, title, required
+        
+def _read_form_line(line):
+    kwargs = ""
+    field_type = ""
+    title=""
+    name=""
+    required=False
+    frm=""    
+    if ':' in line:
+        x = line.rsplit(":", 1)
+        line2 = x[0].strip()
+        frm = x[1].strip()
+        if frm.startswith('0'):
+            field_type='IntegerField'
+            if len(frm)>1:
+               kwargs = 'min_value=0, max_value=%d' % 10**len(frm)
+        elif frm.startswith('9'):
+            field_type='FloatField'
+            if len(frm)>1:
+               kwargs = 'min_value=0, max_value=%d' % 10**len(frm)
+        elif frm.startswith('#'):
+            field_type='DateField'
+        elif frm.startswith('*'):
+            field_type='CharField'
+            if len(frm)>0:
+               kwargs = 'max_length=%d' % len(frm)
+        elif frm.startswith('_'):
+            field_type='CharField'
+            kwargs = 'max_length=%d' % len(frm) 
+        elif frm.startswith('['):
+            field_type='ChoiceField'
+            choices = ",".join(list(["('"+pos+"','"+pos+"')" for pos in frm[1:-1].split(';') if pos]))
+            kwargs = "choices = ["+choices+"]"
+    else:
+        if line.endswith('?'):
+            field_type='BooleanField'
+            line2 = line[:-1].strip()
+        else:
+            field_type='CharField'
+            line2 = line
+    name, title, required = _get_name_and_title(line2)
+    return name, field_type, title, required, kwargs
+    
+def read_from_lines(input_str):
+    ret = []
+    tab = _scan_lines(input_str)
+    for pos in tab:
+        if pos:
+            name, field_type, title, required, kwargs = _read_form_line(pos.strip())
+            if required:
+                r = "True"
+            else:
+                r = "False"
+            s = "    %s = forms.%s(label='%s', required=%s, %s)" % (name, field_type, title, r, kwargs)
+            ret.append(s)
+    return ret     
+
+def form_from_str(input_str, form_name):
+    x = read_from_lines(input_str)
+    return ("class %s(forms.Form):\n" % form_name) + "\n".join(x)
+
+
