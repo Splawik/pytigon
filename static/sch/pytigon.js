89f7440db316839f8bf95976389889131f8c9f55
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2017-10-15 18:31:45 +0200

    small changes

--- static/sch/pytigon.js
+++ static/sch/pytigon.js
@@ -1,5 +1,5 @@
 "use strict";
-// Transcrypt'ed from Python, 2017-10-15 17:49:53
+// Transcrypt'ed from Python, 2017-10-15 18:22:11
 
    var __symbols__ = ['__py3.6__', '__esv5__'];
     var __all__ = {};
