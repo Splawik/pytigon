11ce360e65ac7c0210f06cdef2e686a495a7a38f
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2018-01-02 23:11:57 +0100

    small changes

--- app_pack/_schwiki/wiki/models.py
+++ app_pack/_schwiki/wiki/models.py
@@ -33,6 +33,138 @@ template_content = """
 %s
 """
 
+def _get_wiki_object(page, buf, name):
+    name0 = name.split('_')[0]
+    conf = None
+    x = PageObjectsConf.objects.filter(name=name0)
+    if len(x) > 0:
+        conf = x[0]
+        d = page.get_json_data()
+        if name in d:
+            c = d[name]
+        else:
+            c = ""
+
+        inline_content = norm_indent(buf)
+        if conf.inline_wiki:
+            inline_content = html_from_wiki(page, inline_content)
+
+        context = {'param': c, 'inline_content': inline_content, 'object': conf, 'page': page}
+        if conf.view_dict:
+            exec(conf.view_dict)
+            context = locals()['get_view_dict'](context)
+
+        template_name1 = (conf.app + "/" + conf.name).lower() + "_wikiobj_view.html"
+        template_name2 = "wiki/wikiobj_view.html"
+
+        t = select_template([template_name1, template_name2, ])
+
+        return t.render(context).replace('[[', '{{').replace(']]', '}}').replace('[%', '{%').replace('%]',
+                                                                                                     '%}')
+    else:
+        return ""
+
+
+def _get_markdown_object(buf):
+    return markdown.markdown("\n".join(buf), extras=['tables', 'codehilite'])
+
+
+def html_from_wiki(page, wiki_str):
+    document = []
+    paragraf = []
+    buf = []
+    in_wiki_object = False
+    name = ""
+
+    paragraf_prefix = None
+    paragraf_suffix = None
+    section_close_elements = []
+
+    def write_papragraf():
+        nonlocal in_wiki_object, buf, paragraf_prefix, paragraf_suffix, document, paragraf
+
+        if in_wiki_object:
+            x = _get_wiki_object(page, buf, name)
+            document.append(x)
+            buf = []
+        else:
+            if buf:
+                paragraf.append((buf, True))
+                buf = []
+
+            if paragraf:
+                if paragraf_prefix:
+                    x = paragraf_prefix
+                else:
+                    x = ""
+                for pos in paragraf:
+                    if pos[1]:
+                        x += _get_markdown_object(pos[0])
+                    else:
+                        x += pos[0]
+
+                if paragraf_suffix:
+                    x += paragraf_suffix
+
+                document.append(x)
+
+                paragraf = []
+
+    def write_section():
+        nonlocal section_close_elements, document
+        if section_close_elements:
+            document.append("".join(list(reversed(section_close_elements))))
+        section_close_elements = []
+
+    lines = wiki_str.replace('\r', '').split('\n')
+    for line in lines:
+        if in_wiki_object:
+            if line.startswith(' ') or line.startswith('\t') or not line:
+                buf.append(line)
+                continue
+            else:
+                x = _get_wiki_object(page, buf, name)
+
+                if x.startswith('@@@'):
+                    if '|||' in x:
+                        y  = x[3:].split('|||')
+                        paragraf_prefix = y[0]
+                        paragraf_suffix = y[1]
+                    else:
+                        paragraf_prefix = x[3:]
+                        paragraf_suffix = ""
+                else:
+                    if '|||' in x:
+                        y = x.split('|||')
+                        paragraf.append((y[0], False))
+                        section_close_elements.append(y[1])
+                    else:
+                        paragraf.append((x, False))
+
+                buf = []
+                in_wiki_object = False
+
+        if line.startswith('@'):
+            if buf:
+                #x = _get_markdown_object(buf)
+                paragraf.append((buf, True))
+            buf = []
+            in_wiki_object = True
+            name = line.split(':')[0][1:].strip()
+        elif line.startswith('...') or line.startswith('+++'):
+            write_papragraf()
+            if line.startswith('+++'):
+                write_section()
+                paragraf_prefix = ""
+                paragraf_suffix = ""
+        else:
+            buf.append(line)
+
+    write_papragraf()
+    write_section()
+
+    return "\n".join(document)
+
 
 
 
@@ -64,6 +196,7 @@ class PageObjectsConf( models.Model):
     name = models.CharField('Name', null=False, blank=False, editable=True, max_length=64)
     description = models.CharField('Description', null=True, blank=True, editable=True, max_length=128)
     inline_editing = models.NullBooleanField('Inline editing', null=False, blank=False, editable=True, default=False,)
+    inline_wiki = models.NullBooleanField('Inline wiki', null=False, blank=False, editable=True, default=False,)
     edit_form = models.TextField('Edit form', null=True, blank=True, editable=False, )
     load_fun = models.TextField('Load function', null=True, blank=True, editable=False, )
     save_fun = models.TextField('Save function', null=True, blank=True, editable=False, )
@@ -110,74 +243,7 @@ class Page(JSONModel):
     
     def save(self, *args, **kwargs):
         if self.content_src:
-            
-            def get_wiki_object(buf, name):
-                name0 = name.split('_')[0]
-                conf = None
-                x=PageObjectsConf.objects.filter(name=name0)
-                if len(x)>0:
-                    conf = x[0]
-                    d = self.get_json_data()
-                    if name in d:
-                        c = d[name]
-                    else:
-                        c = ""
-                    
-                    inline_content = norm_indent(buf)
-                
-                    context = { 'param': c, 'inline_content': inline_content, 'object': conf, 'page': self }    
-                    if conf.view_dict:
-                        exec(conf.view_dict)
-                        context = locals()['get_view_dict'](context)
-                        
-                    template_name1 = (conf.app + "/" + conf.name).lower()+"_wikiobj_view.html"
-                    template_name2 = "wiki/wikiobj_view.html"
-                
-                    t = select_template([template_name1,template_name2,])
-                
-                    return t.render(context).replace('[[','{{').replace(']]', '}}').replace('[%', '{%').replace('%]', '%}')
-                else:
-                    return ""
-                
-            def get_markdown_object(buf):
-                return markdown.markdown("\n".join(buf), extras=['tables', 'codehilite'])
-                
-            blocks = []
-            buf = []
-            level = 0
-            in_wiki_object = False
-            name = ""
-    
-            lines = self.content_src.replace('\r','').split('\n')
-            for line in lines:
-                if in_wiki_object:
-                    if line.startswith(' ') or not line:
-                        buf.append(line)
-                        continue
-                    else:
-                        x = get_wiki_object(buf,name)
-                        blocks.append(x)
-                        buf = []
-                        in_wiki_object = False
-                        
-                if line.startswith('@'):
-                    if buf:
-                        x = get_markdown_object(buf)
-                        blocks.append(x)
-                    buf = []
-                    in_wiki_object = True
-                    name = line.split(':')[0][1:].strip()
-                else:
-                    buf.append(line)
-                    
-            if buf:
-                if in_wiki_object:
-                    x = get_wiki_object(buf,name)
-                else:
-                    x = get_markdown_object(buf)
-                blocks.append(x)
-                
-            content = "\n".join(blocks)
+            content = html_from_wiki(self, self.content_src)
         else:
             content = ""
         t = Template(template_content % content)
