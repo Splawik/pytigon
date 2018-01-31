f469eceb5cfe1f8b92c4b614f6fc07b2f741a284
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2018-01-31 22:39:43 +0100

    simple changes

--- app_pack/_schall/apps.py
+++ app_pack/_schall/apps.py
@@ -1,2 +1,2 @@
-APPS=['schdevtools.schbuilder','schpytigondoc.pytigon_doc','_schwiki.wiki','_schtasks.tasks','_schtools.commander','_schtools.simplescripts','_schtools.tools','_schtools.attachements','_schtools.log','_schtools.schsession','_schdata.schelements','_schdata.schreports',]
+APPS=['schdevtools.schbuilder','_schwiki.wiki','_schtasks.tasks','_schtools.commander','_schtools.simplescripts','_schtools.tools','_schtools.attachements','_schtools.log','_schtools.schsession','_schdata.schelements','_schdata.schreports',]
 PUBLIC = False
