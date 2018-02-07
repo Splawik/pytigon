912e3a1f85b212fb3fda958cc9189f34b4ecf062
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2018-02-07 18:23:14 +0100

    simple changes

--- install_in_docker.sh
+++ install_in_docker.sh
@@ -1,6 +1,6 @@
 pip3 install -r requirements.txt
 cd app_pack
-zip ../install/app_pack.zip *
+zip -r ../install/app_pack.zip *
 cd ..
 curl -L -o ./ext_prg/tcc-0.9.26.tar.bz2 http://download.savannah.gnu.org/releases/tinycc/tcc-0.9.26.tar.bz2
 tar xjf ./ext_prg/tcc-0.9.26.tar.bz2 -C ./ext_prg/
