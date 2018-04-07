fa39a986c6897719cb530444e305618ea512306c
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2018-04-07 19:02:16 +0200

    simple changes

---
+++ app_pack/schsetup/apps.py
@@ -0,0 +1,3 @@
+APPS=['schinstall','_schtools.schsimplescripts','_schtools.schtools','_schwiki.schwiki','_schtools.schcommander','_schtasks.schtasks',]
+PUBLIC = False
+MAIN_APP_PACK = False
---
+++ app_pack/schsetup/schinstall/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+
+ModuleTitle = _('main tools')
+Title = _('Pytigon setup')
+Perms = True
+Index = 'None'
+Urls  = (
+    ('form/upload_ptig/?schtml=desktop',_('Install pytigon application (*.ptig)'),None,'fa://rocket.png'),
+)
+UserParam = {}
