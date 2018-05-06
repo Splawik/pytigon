355cfdc00d058a366b2e954fb76dbaa9b1eb73aa
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2018-05-06 20:05:12 +0200

    simple changes

--- app_pack/_schall/apps.py
+++ app_pack/_schall/apps.py
@@ -1,3 +1,3 @@
-APPS=['schdevtools.schbuilder','schpytigondemo.schsimplecontrolsdemo','schpytigondemo.schhtmllib','schpytigondemo.schtabledemo','schpytigondemo.schfilemanagerdemo','schpytigondemo.schcommandshelldemo','schpytigondemo.schspreadsheetdemo','schpytigondemo.schtasksdemo','schpytigondemo.schadvcontrolsdemo','_schwiki.schwiki','_schtasks.schtasks','_schtools.schcommander','_schtools.schsimplescripts','_schtools.schtools','_schtools.schattachements','_schtools.schlog','_schdata.schelements','_schdata.schreports',]
+APPS=['schdevtools.schbuilder','_schwiki.schwiki','_schtasks.schtasks','_schtools.schcommander','_schtools.schsimplescripts','_schtools.schtools','_schtools.schattachements','_schtools.schlog','_schdata.schelements','_schdata.schreports',]
 PUBLIC = False
 MAIN_APP_PACK = False
