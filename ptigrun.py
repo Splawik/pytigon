ec53d070c293568a18c8bc503b8036a1204e8e73
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2018-08-18 17:51:37 +0200

    appimamge support

--- ptigrun.py
+++ ptigrun.py
@@ -22,6 +22,7 @@ import subprocess
 import os
 
 from schcli.pytigon import main
+from schlib.schtools.tools import get_executable
 
 def run():
     if len(sys.argv)>1 and sys.argv[1].startswith('manage'):
@@ -37,10 +38,10 @@ def run():
             path2 = os.path.join(base_path, "app_pack")
             path3 = os.path.join(path2, app)
             os.chdir(path3)
-            subprocess.run(["../../python/bin/python", "manage.py"] + sys.argv[2:])            
+            subprocess.run([get_executable(), "manage.py"] + sys.argv[2:])
             os.chdir(base_path)
         else:
-            subprocess.run(["./python/bin/python", "manage.py"] + sys.argv[2:])
+            subprocess.run([get_executable(), "manage.py"] + sys.argv[2:])
     else:
         main(sys.argv[1:])
 
