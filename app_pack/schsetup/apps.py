c2f45d16d8ffa31181750fe41bca30d89e05c97c
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2018-10-14 23:23:32 +0200

    small changes

--- app_pack/schsetup/apps.py
+++ app_pack/schsetup/apps.py
@@ -1,3 +1,3 @@
 APPS=['schinstall','schadmin','_schtools.schsimplescripts','_schtools.schtools','_schwiki.schwiki','_schtools.schcommander','_schtasks.schtasks',]
-PUBLIC = False
+PUBLIC = True
 MAIN_APP_PACK = False
