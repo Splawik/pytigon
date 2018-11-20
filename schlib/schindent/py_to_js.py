54a9e7beca10a213a2acfd069d49b4b10e4f81de
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2018-11-21 00:09:27 +0100

    small updates

--- schlib/schindent/py_to_js.py
+++ schlib/schindent/py_to_js.py
@@ -42,10 +42,10 @@ def compile(python_code, temp_dir=None):
     else:
         tmp = tempDir
 
-    compilerPath = [tmp, programDir, modulesDir] + sys.path
+    compilerPath = [programDir, modulesDir, tmp] + sys.path
 
     src = os.path.join(tmp, "input.py")
-    dest = os.path.join(os.path.join(tmp, "__target__"), "input.js")
+    dest = os.path.join(os.path.join(tmp, "__javascript__"), "input.mod.js")
 
     if os.path.exists(dest):
         os.remove(dest)
@@ -54,42 +54,68 @@ def compile(python_code, temp_dir=None):
         pyinput.write(python_code)
 
     x = {
-        'source': src.replace(".py",""),
-        'nomin': True,
+        'source': src, 
+        'anno': False, 
+        'build': False, 
+        'complex': False,
+        'docat': False, 
+        'dcheck': False, 
+        'dassert': False, 
+        'dextex': False, 
+        'dmap': False, 
+        'dtree': False, 
+        'dstat': False, 
+        'esv': None, 
+        'fcall': False, 
+        'gen': False,
+        'iconv': False, 
+        'jskeys': False, 
+        'jsmod': False, 
+        'kwargs': False, 
+        'license': False, 
+        'map': False, 
+        'nomin': True, 
+        'opov': False, 
+        'parent': None, 
+        'run': False, 
+        'symbols': None, 
+        'tconv': False, 
+        'verbose': False, 
+        'ext': None, 
+        'star': False,
+        'keycheck': False,
         'dnostrip': True,
         'jscall': True
     }
 
-    tmp_argv =  sys.argv
-    sys.argv = [sys.argv[0],]
-    try:
-        utils.commandArgs.parse()
-    except:
-        pass
-    sys.argv = tmp_argv
-
     utils.commandArgs.__dict__.update (x)
+
     __symbols__ = []
     __symbols__.append ('__py{}.{}__'.format (* sys.version_info [:2]))
+    #if utils.commandArgs.esv:
+    #    __symbols__.append ('__esv{}__'.format (utils.commandArgs.esv))
+    #else:
     __symbols__.append ('__esv{}__'.format (utils.defaultJavaScriptVersion))
-    __envir__ = utils.Any()
-    with tokenize.open(f'{modulesDir}/org/transcrypt/__envir__.js') as envirFile:
-        exec(envirFile.read());
-    __envir__.executor_name = __envir__.interpreter_name
+
+    #__envir__ = utils.Any()
+    #with tokenize.open(f'{modulesDir}/org/transcrypt/__envir__.js') as envirFile:
+    #    exec(envirFile.read());
+    #__envir__.executor_name = __envir__.interpreter_name
+
     error = False
-
     try:
-        compiler.Program(compilerPath, __symbols__, __envir__)
+        compiler.Program (compilerPath, compilerPath, __symbols__)
+        #compiler.Program(compilerPath, __symbols__, __envir__)
         with open(dest,"rt") as pyoutput:
-            s = pyoutput.read()
-            #s = ret.split('(function () {\n', 1)[1]
-            #s = s.rsplit('}) ();', 1)[0]
-
+            ret = pyoutput.read()        
+            s = ret.split('(function () {\n', 1)[1]
+            s = s.rsplit('}) ();', 1)[0]            
+            
             tab = []
             for line in s.split("\n"):
                 tab.append(line[2:])
             ret = "\n".join(tab)
-
+            
     except Exception as exception:
         error = True
         tab = []
@@ -121,5 +147,7 @@ def compile(python_code, temp_dir=None):
                         tab.append(line)
                 else:
                     tab.append(line)
+                    
         ret = "\n".join(tab)
+        
     return (error, ret)
