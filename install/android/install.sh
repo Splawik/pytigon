71020d15e5042d28c6344a10794509f9ada0e3e3
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2018-12-23 17:53:55 +0100

    small updates

---
+++ install/android/install.sh
@@ -0,0 +1,26 @@
+if [ ! -d ./pytigon  ]; then
+    git clone https://github.com/Splawik/pytigon.git
+    mkdir pytigon/_android_src/recipes/none
+fi
+
+if [ ! -d ./python-for-android  ]; then
+    git clone https://github.com/kivy/python-for-android.git
+    yes | cp pytigon/install/android/Dockerfile python-for-android/Dockerfile
+    cd python-for-android
+    docker build --tag p4apy3 .
+    mkdir home_data
+    cd ..
+fi
+
+if [ ! -d ./venv  ]; then
+    virtualenv --python=python3.7 venv
+    venv/bin/python -m pip install -r pytigon/install/android/requirements.txt 
+else
+    venv/bin/python -m pip install --upgrade -r pytigon/install/android/requirements.txt 
+fi 
+
+yes | cp -Rf venv/lib/python3.7/site-packages pytigon/_android
+
+cd pytigon
+bash build_android.sh
+cd ..
