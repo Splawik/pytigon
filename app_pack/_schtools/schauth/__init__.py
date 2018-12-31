5b96537133d1ceb0ab0afdd9fdef12d3293199a8
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2018-12-31 08:17:02 +0100

    small updates

---
+++ app_pack/_schtools/schauth/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+
+ModuleTitle = _('Authorization')
+Title = _('Authorization')
+Perms = True
+Index = 'None'
+Urls  = (
+    ('table/UrlWithAuth/-/form/list/?schtml=desktop',_('Urls with auth'),None,'png://apps/system-users.png'),
+)
+UserParam = {}
---
+++ app_pack/_schtools/schauth/views.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+from django import forms
+from django.template.loader import render_to_string
+from django.template import Context, Template
+from django.template import RequestContext
+from django.conf import settings
+from django.views.generic import TemplateView
+
+from schlib.schviews.form_fun import form_with_perms
+from schlib.schviews.viewtools import dict_to_template, dict_to_odf, dict_to_pdf, dict_to_json, dict_to_xml
+from schlib.schviews.viewtools import render_to_response
+from schlib.schdjangoext.tools import make_href
+
+from django.utils.translation import ugettext_lazy as _
+
+from . import models
+import os
+import sys
+import datetime
+
+from django.urls import reverse
+from django.contrib.auth import login
+from django.contrib.auth.models import User 
+
+
+
+
+
+
+
+
+
+
+def auth(request, key, path):
+    
+    objects = models.UrlWithAuth.objects.filter(key=key)
+    if len(objects) == 1:
+        username = objects[0].username
+        users = User.objects.filter(username=username)
+        if len(users)==1:
+            login(request, users[0])
+            if path:
+                new_url = reverse('start')+path
+            else:
+                new_url = reverse('start')+objects[0].redirect_to
+            return HttpResponseRedirect(new_url)
+    
+    new_url = reverse('start')
+    return HttpResponseRedirect(new_url)
+    
+
+
+ 
