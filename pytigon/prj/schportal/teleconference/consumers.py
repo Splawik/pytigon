f0f2458477c8b2feffe571e10dd04a45208317ad
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2020-03-30 23:31:04 +0200

    webrtc support

--- pytigon/prj/schportal/teleconference/consumers.py
+++ pytigon/prj/schportal/teleconference/consumers.py
@@ -38,30 +38,56 @@ class teleconference(AsyncWebsocketConsumer):
         slug = "default"
     
         await self.accept()
-    
+        
+        status = 0
         if slug not in GLOBAL_NODES:
             GLOBAL_NODES[slug] = Node(slug, [self])
         else:
-            GLOBAL_NODES[slug].clients.append(self)
+            if GLOBAL_NODES[slug].clients[0]==None:
+               GLOBAL_NODES[slug].clients[0] = self
+            else:
+                GLOBAL_NODES[slug].clients.append(self)
+                if len(GLOBAL_NODES[slug].clients) > 2:
+                    status = 2
+                else:
+                    status = 1
     
         self.current_node = GLOBAL_NODES[slug]
     
-        if len(self.current_node.clients) == 1:
-            await self.send(text_data="owner")
-        elif len(self.current_node.clients) > 2:
-            await self.send(text_data="magic_overload")
-        else:
-            await self.send(text_data="guest")
+        #await self.send(text_data=["owner", "guest", "magic_overload"][status])
+        await self.send(text_data=["owner", "guest", "guest"][status])
+    
+        #if len(self.current_node.clients) == 1:
+        #    await self.send(text_data="owner")
+        #elif len(self.current_node.clients) > 2:
+        #    await self.send(text_data="magic_overload")
+        #else:
+        #    await self.send(text_data="guest")
      
     
     async def disconnect(self, close_code):
-        self.current_node.clients.remove(self)
+        if self.current_node.clients[0] == self:
+           self.current_node.clients[0] = None
+        else:
+            self.current_node.clients.remove(self)
+        print("DDDDDDDDDDDDDDDDDIIIIIIIIIIIISSSSSSSSSSCONNECT")
     
     async def receive(self, text_data, bytes_data=None):
-        for client in self.current_node.clients:
-            if client is self:
-                continue
-            await client.send(text_data=text_data)    
+        if self == self.current_node.clients[0]:
+            for client in self.current_node.clients[1:]:
+                if client == None:
+                    continue
+                await client.send(text_data=text_data)    
+        else:
+            if self.current_node.clients[0]:
+                await self.current_node.clients[0].send(text_data=text_data)
+            
+        #for client in self.current_node.clients:
+        #    if client is self:
+        #        continue
+        #    if client == None:
+        #        continue
+        #    await client.send(text_data=text_data)    
     
     
     
