81c40cecebc6bfe22efd2eeb3abf017ca8f087f8
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2020-12-21 18:27:49 +0100

    small changes

---
+++ pytigon/prj/_schtools/schtasks/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+
+ModuleTitle = _('Tasks')
+Title = _('Tasks')
+Perms = False
+Index = 'None'
+Urls  = (
+)
+UserParam = {}
