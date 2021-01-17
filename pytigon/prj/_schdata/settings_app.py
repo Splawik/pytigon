e9bfe76697d9f60e7c6d0e6cbf2f9bee86e9b3b4
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2021-01-17 23:46:18 +0100

    Transcrypt -> pscript

--- pytigon/prj/_schdata/settings_app.py
+++ pytigon/prj/_schdata/settings_app.py
@@ -131,7 +131,7 @@ try:
 except:
     pass
 
-GEN_TIME = '2020.12.28 19:49:33'
+GEN_TIME = '2021.01.17 18:20:17'
 
 for key, value in os.environ.items():
     if key.startswith("PYTIGON_"):
--- pytigon/prj/_schdata/static/_schdata/js/subreport.js
+++ pytigon/prj/_schdata/static/_schdata/js/subreport.js
@@ -1,43 +1,50 @@
-// Transcrypt'ed from Python, 2020-12-28 20:49:34
-import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__,  __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from '../../pytigon_js/org.transcrypt.__runtime__.js';
-var __name__ = '__main__';
-export var subreport_dragstart = function (ev) {
-	ev.dataTransfer.setData ('text', ev.target.getAttribute ('name'));
+var subreport_dragstart, subreport_drop, subreport_ondragenter, subreport_ondragleave, subreport_ondragover;
+subreport_dragstart = function flx_subreport_dragstart (ev) {
+    ev.dataTransfer.setData("text", ev.target.getAttribute("name"));
+    return null;
 };
-window.subreport_dragstart = subreport_dragstart;
-export var subreport_drop = function (ev, base_path) {
-	ev.preventDefault ();
-	if (ev.target.tagName == 'LABEL') {
-		var target = ev.target;
-	}
-	else {
-		var target = ev.target.parentElement;
-	}
-	var data = ev.dataTransfer.getData ('text');
-	var data2 = target.getAttribute ('name');
-	if (data2 != data) {
-		var href = ((((base_path + 'schreports/table/Report/') + data) + '/') + data2) + '/action/move_to/';
-		ajax_get (href, standard_on_data (jQuery (target), href));
-	}
-};
-window.subreport_drop = subreport_drop;
-export var subreport_ondragenter = function (ev) {
-	if (ev.target.tagName == 'LABEL') {
-		jQuery (ev.target).addClass ('bg-success');
-	}
-};
-window.subreport_ondragenter = subreport_ondragenter;
-export var subreport_ondragleave = function (ev) {
-	if (ev.target.tagName == 'LABEL') {
-		jQuery (ev.target).removeClass ('bg-success');
-	}
-};
-window.subreport_ondragleave = subreport_ondragleave;
-export var subreport_ondragover = function (ev) {
-	if (ev.target.tagName == 'LABEL') {
-		ev.preventDefault ();
-	}
-};
-window.subreport_ondragover = subreport_ondragover;
 
-//# sourceMappingURL=input.map
+window.subreport_dragstart = subreport_dragstart;
+subreport_drop = function flx_subreport_drop (ev, base_path) {
+    var data, data2, href, target;
+    ev.preventDefault();
+    if (_pyfunc_op_equals(ev.target.tagName, "LABEL")) {
+        target = ev.target;
+    } else {
+        target = ev.target.parentElement;
+    }
+    data = ev.dataTransfer.getData("text");
+    data2 = target.getAttribute("name");
+    if ((!_pyfunc_op_equals(data2, data))) {
+        href = ((((base_path + "schreports/table/Report/") + data) + "/") + data2) + "/action/move_to/";
+        ajax_get(href, standard_on_data(jQuery(target), href));
+    }
+    return null;
+};
+
+window.subreport_drop = subreport_drop;
+subreport_ondragenter = function flx_subreport_ondragenter (ev) {
+    if (_pyfunc_op_equals(ev.target.tagName, "LABEL")) {
+        (jQuery(ev.target).addClass)("bg-success");
+    }
+    return null;
+};
+
+window.subreport_ondragenter = subreport_ondragenter;
+subreport_ondragleave = function flx_subreport_ondragleave (ev) {
+    if (_pyfunc_op_equals(ev.target.tagName, "LABEL")) {
+        (jQuery(ev.target).removeClass)("bg-success");
+    }
+    return null;
+};
+
+window.subreport_ondragleave = subreport_ondragleave;
+subreport_ondragover = function flx_subreport_ondragover (ev) {
+    if (_pyfunc_op_equals(ev.target.tagName, "LABEL")) {
+        ev.preventDefault();
+    }
+    return null;
+};
+
+window.subreport_ondragover = subreport_ondragover;
+export {subreport_dragstart, subreport_drop, subreport_ondragenter, subreport_ondragleave, subreport_ondragover};
