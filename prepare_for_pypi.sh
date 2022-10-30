4a95f4517dd98df8a1db3e993c28932db01e842c
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2022-10-30 11:17:58 +0100

    small changes

--- prepare_for_pypi.sh
+++ prepare_for_pypi.sh
@@ -3,17 +3,23 @@ mkdir /tmp/pytigon
 cd pytigon
 #python ptig.py manage_schdevtools prepare_installer_files 
 export DATA_PATH=/tmp/pytigon
+python ptig.py manage_schdevtools makeallmigrations
 python ptig.py manage_schdevtools migrate
 python ptig.py manage_schdevtools createautouser
 python ptig.py manage_schdevtools import_projects
+python ptig.py manage_schpytigondemo makeallmigrations
 python ptig.py manage_schpytigondemo migrate
 python ptig.py manage_schpytigondemo createautouser
+python ptig.py manage_schsetup makeallmigrations
 python ptig.py manage_schsetup migrate
 python ptig.py manage_schsetup createautouser
+python ptig.py manage_schwebtrapper makeallmigrations
 python ptig.py manage_schwebtrapper migrate
 python ptig.py manage_schwebtrapper createautouser
+python ptig.py manage_scheditor makeallmigrations
 python ptig.py manage_scheditor migrate
 python ptig.py manage_scheditor createautouser
+python ptig.py manage_schportal makeallmigrations
 python ptig.py manage_schportal migrate
 python ptig.py manage_schportal createautouser
 echo -n "[DEFAULT]
