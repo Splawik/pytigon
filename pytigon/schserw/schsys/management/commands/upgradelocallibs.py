08c44cf7a935d795041904155e5aa0a5b3518834
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2023-05-05 17:39:53 +0200

    small changes

---
+++ pytigon/schserw/schsys/management/commands/upgradelocallibs.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand, CommandError
+from pytigon_lib.schtools.install_init import upgrade_local_libs
+
+
+class Command(BaseCommand):
+    help = "Upgrade local libs"
+
+    def handle(self, *args, **options):
+        upgrade_local_libs()
