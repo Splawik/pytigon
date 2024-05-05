3fe33e34a3a885297418c7c5c6a509ce49716c98
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2024-05-05 13:41:25 +0200

    v0.240505

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="pytigon",
-    version="0.240428",
+    version="0.240505",
     description="Pytigon",
     author="Sławomir Chołaj",
     author_email="slawomir.cholaj@gmail.com",
