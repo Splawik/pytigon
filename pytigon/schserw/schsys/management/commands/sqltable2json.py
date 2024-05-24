0cc1690bd01e36d1dce854a2eeec5f51599df90a
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2024-05-25 00:06:34 +0200

    sqltable2json management command

---
+++ pytigon/schserw/schsys/management/commands/sqltable2json.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db import connection
+
+import json
+
+
+def get_table(table_name):
+    with connection.cursor() as cursor:
+        cursor.execute("select * from %s" % table_name)
+        r = [
+            dict((cursor.description[i][0], value) for i, value in enumerate(row))
+            for row in cursor.fetchall()
+        ]
+        return r
+
+
+class Command(BaseCommand):
+    help = "Export sql table to json"
+
+    def add_arguments(self, parser):
+        parser.add_argument("table_name", nargs="?", help="Table name to export")
+        parser.add_argument(
+            "--output",
+            help="save output to file",
+        )
+
+    def handle(self, *args, **options):
+        if not options["table_name"]:
+            raise CommandError("required table name")
+
+        print(options)
+        if "output" in options and options["output"]:
+            o = options["output"]
+        else:
+            o = None
+
+        t = get_table(options["table_name"])
+        if o:
+            with open(o, "wt") as f:
+                f.write(json.dumps(t))
+        else:
+            print(json.dumps(t))
