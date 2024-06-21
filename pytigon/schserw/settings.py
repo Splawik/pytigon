920a3835c14023f6f278a6333185868a75855d44
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2024-06-21 10:55:41 +0200

    Better Prometheus support

--- pytigon/schserw/settings.py
+++ pytigon/schserw/settings.py
@@ -736,3 +736,24 @@ def finish(settings):
     if ENV("PROMETHEUS_ENABLED"):
         MIDDLEWARE.insert(0, "django_prometheus.middleware.PrometheusBeforeMiddleware")
         MIDDLEWARE.append("django_prometheus.middleware.PrometheusAfterMiddleware")
+        if "default" in DATABASES:
+            engine = DATABASES["default"]["engine"]
+            if engine in (
+                "django.db.backends.sqlite3",
+                "django.db.backends.postgresql",
+                "django.db.backends.mysql",
+            ):
+                DATABASES["default"]["engine"] = engine.replace(
+                    "django.db.backends", "django_prometheus.db.backends"
+                )
+        if "default" in CACHES:
+            backend = CACHES["default"]["BACKEND"]
+            if backend in (
+                "django.core.cache.backends.memcached.PyMemcacheCache",
+                "django.core.cache.backends.memcached.PyLibMCCache",
+                "django.core.cache.backends.redis.RedisCache",
+                "django.core.cache.backends.filebased.FileBasedCache",
+            ):
+                CACHES["default"]["BACKEND"] = backend.replace(
+                    "django.core.cache.backends", "django_prometheus.cache.backends"
+                )
