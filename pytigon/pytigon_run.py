6345d4e08810e015624b8b029d36b9caac567257
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2025-03-02 14:58:25 +0100

    small change

--- pytigon/pytigon_run.py
+++ pytigon/pytigon_run.py
@@ -180,7 +180,7 @@ def run(param=None):
             import django
 
             django.setup()
-            getattr(module, "main")()
+            return getattr(module, "main")()
 
     elif len(argv) > 1 and argv[1].startswith("runserver_"):
         x = argv[1].split("_", 1)
