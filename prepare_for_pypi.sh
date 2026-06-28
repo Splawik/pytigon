b4a72e279a49606b9f9c0d703f02596078af69be
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2026-06-28 21:36:14 +0200

    refactor(project): simplify shell script commands and update project component definitions

--- prepare_for_pypi.sh
+++ prepare_for_pypi.sh
@@ -1,30 +1,30 @@
 rm -rf /tmp/pytigon
 mkdir /tmp/pytigon
 cd pytigon
-python ptig.py manage_schdevtools prepare_installer_files 
+ptig manage_schdevtools prepare_installer_files 
 export DATA_PATH=/tmp/pytigon
-python ptig.py manage__schdata makeallmigrations
-python ptig.py manage__schtools makeallmigrations
-python ptig.py manage__schwiki makeallmigrations
-python ptig.py manage_schdevtools makeallmigrations
-python ptig.py manage_schdevtools migrate
-python ptig.py manage_schdevtools createautouser
-python ptig.py manage_schdevtools import_projects
-python ptig.py manage_schpytigondemo makeallmigrations
-python ptig.py manage_schpytigondemo migrate
-python ptig.py manage_schpytigondemo createautouser
-python ptig.py manage_schmanage makeallmigrations
-python ptig.py manage_schmanage migrate
-python ptig.py manage_schmanage createautouser
-python ptig.py manage_schwebtrapper makeallmigrations
-python ptig.py manage_schwebtrapper migrate
-python ptig.py manage_schwebtrapper createautouser
-python ptig.py manage_scheditor makeallmigrations
-python ptig.py manage_scheditor migrate
-python ptig.py manage_scheditor createautouser
-python ptig.py manage_schportal makeallmigrations
-python ptig.py manage_schportal migrate
-python ptig.py manage_schportal createautouser
+ptig manage__schdata makeallmigrations
+ptig manage__schtools makeallmigrations
+ptig manage__schwiki makeallmigrations
+ptig manage_schdevtools makeallmigrations
+ptig manage_schdevtools migrate
+ptig manage_schdevtools createautouser
+ptig manage_schdevtools import_projects
+ptig manage_schpytigondemo makeallmigrations
+ptig manage_schpytigondemo migrate
+ptig manage_schpytigondemo createautouser
+ptig manage_schmanage makeallmigrations
+ptig manage_schmanage migrate
+ptig manage_schmanage createautouser
+ptig manage_schwebtrapper makeallmigrations
+ptig manage_schwebtrapper migrate
+ptig manage_schwebtrapper createautouser
+ptig manage_scheditor makeallmigrations
+ptig manage_scheditor migrate
+ptig manage_scheditor createautouser
+ptig manage_schportal makeallmigrations
+ptig manage_schportal migrate
+ptig manage_schportal createautouser
 echo -n "[DEFAULT]
 GEN_TIME='" > /tmp/pytigon/install.ini
 echo -n $(date +"%Y-%m-%d %H:%M:%S") >> /tmp/pytigon/install.ini
