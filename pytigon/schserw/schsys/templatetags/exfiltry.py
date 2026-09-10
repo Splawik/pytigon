6d2faade34b3cdad7c24b879bebad9699e8f2399
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2026-09-10 20:30:38 +0200

    chore: update version to 0.260910 in pyproject.toml and __init__.py; add bool2icon filter to convert boolean values to icon representation

--- pytigon/schserw/schsys/templatetags/exfiltry.py
+++ pytigon/schserw/schsys/templatetags/exfiltry.py
@@ -516,6 +516,15 @@ def d_isoformat(value):
     return value.isoformat()[:10] if value else ""
 
 
+@register.filter(name="bool2icon")
+def bool2icon(value):
+    """Converts a boolean value to an icon representation."""
+    if value:
+        return """<svg xmlns="http://w3.org" width="20" height="20" fill="currentColor" class="bi bi-check-circle-fill text-success" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>"""
+    else:
+        return """<svg xmlns="http://w3.org" width="20" height="20" fill="currentColor" class="bi bi-x-circle-fill text-danger" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/></svg>"""
+
+
 @register.filter(name="one_line_block")
 def one_line_block(value):
     """Cleans the value by removing unnecessary spaces and characters."""
